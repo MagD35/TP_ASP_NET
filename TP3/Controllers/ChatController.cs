80bf88ce23d9f7b35c391f5f27e9251a9b18e891
MagD35 <bellule35@outlook.fr>
2021-04-21 12:12:26 +0200

    tp3

---
+++ TP3/Controllers/ChatController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP3.Controllers
+{
+    public class ChatController : Controller
+    {
+        public List<Models.Chat> chats = Models.Chat.GetMeuteDeChats();
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)        {
+
+            Models.Chat chat = chats.Where(c => c.Id == id).FirstOrDefault();
+
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Models.Chat chat = chats.Where(c => c.Id == id).FirstOrDefault();
+            return View();
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Models.Chat chat = chats.Where(c => c.Id == id).FirstOrDefault();
+                chats.Remove(chat);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
