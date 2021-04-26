64bd275c7405ecc74b18e521402f25a504f9503a
MagD35 <bellule35@outlook.fr>
2021-04-26 22:35:03 +0200

    tp6

---
+++ TP6/Data/TP6Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TP6.Data
+{
+    public class TP6Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TP6Context() : base("name=TP6Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<DojoClassLibrary.Samourai> Samourais { get; set; }
+
+        public System.Data.Entity.DbSet<DojoClassLibrary.Arme> Armes { get; set; }
+    }
+}
---
+++ TP6/Models/SamouraiViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using DojoClassLibrary;
+
+namespace TP6.Models
+{
+    public class SamouraiViewModel
+    {
+        [Required]
+        public Samourai Samourai { get; set; }
+        public List<Arme> Armes { get; set; }
+
+        public int? IdArme { get; set; }
+    }
+}
